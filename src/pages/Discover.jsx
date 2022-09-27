cee8c5cf0211c697de9020e87d0ff7f027e7e454
shrutiupadhyayy <67064010+shrutiupadhyayy@users.noreply.github.com>
2022-09-27 13:16:33 +0530

    first

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,44 +1,50 @@
-import { Error, Loader, SongCard } from '../components';
-import { genres } from '../assets/constants';
-
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
-    const genreTitle ='Pop';
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = "Pop";
 
-    return (
-        <div className="flex flex-col">
-          <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-          <h2 className="font-bold text-3xl text-white text-left"
-          >Discover {genreTitle}</h2>
-          <select 
-            onChange={() => {}}
-            value=""
-            className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
-           >
-            {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+  
+  if (isFetching) return <Loader title="Loading songs..." />;
 
+  if (error) return <Error />;
 
-          </select>
-         </div> 
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
 
-         {/* WRAPPER FOR THE SONNGS*/}
-         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+      {/* WRAPPER FOR THE SONNGS*/}
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
           <SongCard
             key={song.key}
             song={song}
-            isPlaying={isPlaying}
-            activeSong={activeSong}
-            data={data}
+            // isPlaying={isPlaying}
+            // activeSong={activeSong}
+            // data={data}
             i={i}
           />
         ))}
       </div>
-
-        </div>
-    )
-}
-
-
+    </div>
+  );
+};
 
 export default Discover;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+ 
+ export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+      baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+      prepareHeaders: (headers) => {
+        headers.set('X-RapidAPI-Key', 'e8f6e5dca2mshf358f8696f6cb19p187c43jsnc41e6f838af6');
+        return headers;
+      },
+   }),
+   endpoints: (builder) => ({ //building all the end points of an API that we are going to call
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    // getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+    // getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    // getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    // getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    // getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    // getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+   }),
+
+  }); //calling the createApi function, we are providing just one object inside it, API NEEDS TO HAVE A REDUCER PATH, that will just be the name pof the API
+
+  export const {
+    useGetTopChartsQuery,
+    // useGetSongsByGenreQuery,
+    // useGetSongsByCountryQuery,
+    // useGetSongsBySearchQuery,
+    // useGetArtistDetailsQuery,
+    // useGetSongDetailsQuery,
+    // useGetSongRelatedQuery,
+  } = shazamCoreApi;
